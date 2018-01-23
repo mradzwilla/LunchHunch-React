78e490f3cf10e6e128e79ad166cfe8dc4e021dc9
mradzwilla <mradzwilla@gmail.com>
2018-01-22 23:43:39 -0500

    Heroku...

--- start-client.js
+++ start-client.js
@@ -1,4 +1,4 @@
 //Moves into the client directory and launches React app
-const args = [ 'build' ];
+const args = [ 'run-script build' ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').spawn('npm', args, opts);
