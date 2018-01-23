9674507d0ec893ea4b62271205ffc45307c46ac2
mradzwilla <mradzwilla@gmail.com>
2018-01-23 00:08:57 -0500

    Herokuing

--- start-client.js
+++ start-client.js
@@ -1,4 +1,4 @@
 //Moves into the client directory and launches React app
-const args = [ 'prep' ];
+const args = [ 'start' ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').spawn('npm', args, opts);
